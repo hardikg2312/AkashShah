e4486373affbf1b4022808eceaee6392609f6339
Hardik Gondaliya <hardikg23@gmail.com>
2015-07-04 14:09:25 +0530

    first commit

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dealduniya-in'
+  config.api_key = '542475821273583'
+  config.api_secret = 'RBy3zVA2UEZU4bB5jQ4Xffs6YKg'
+  config.cdn_subdomain = true
+end
